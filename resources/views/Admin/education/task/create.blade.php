675b7a898f5683399aa5ad92cf1f78465d37fb57
Andriy <53307226+Zaba-web@users.noreply.github.com>
2020-12-27 11:49:35 +0200

    'Back' button fix

--- resources/views/Admin/education/task/create.blade.php
+++ resources/views/Admin/education/task/create.blade.php
@@ -16,7 +16,7 @@
 <div class="content-wrapper">
     <small class='back-container'>
         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"width="16" height="16"viewBox="0 0 172 172"style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M116.45833,9.63021l-64.5,71.66667l-4.25521,4.70313l4.25521,4.70313l64.5,71.66667l10.75,-9.40625l-60.24479,-66.96354l60.24479,-66.96354z"></path></g></g></svg>
-        <a href="{{url()->previous()}}" class='white'>Повернутись назад</a>
+        <a href="{{url("/admin/education/theme/manage/{$theme->id}")}}" class='white'>Повернутись назад</a>
     </small>
     <br>
     <br>
