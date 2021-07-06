9701c01541b209f3f4ad23b4cb8adbeb34c97750
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-06 15:43:46 +0300

    Routes for API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+header('Access-Control-Allow-Origin: http://localhost:8080');
+header('Access-Control-Allow-Headers: origin, x-requested-with, content-type');
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/groups', "Admin\GroupController@list");
+
+Route::post('/register', "RegisterController@registerUser");
+Route::post('/login', "LoginController@login");
+
+// Route::middleware('auth:api')->get('/user', function(Request $request){
+//     return $request->user();
+// });
+
