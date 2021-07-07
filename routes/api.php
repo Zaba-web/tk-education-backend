347dc551054f935a7faa7554ee65dc60ec090050
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-07 03:07:25 +0300

    Add new routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 header('Access-Control-Allow-Origin: http://localhost:8080');
-header('Access-Control-Allow-Headers: origin, x-requested-with, content-type');
+header('Access-Control-Allow-Headers: origin, x-requested-with, content-type, authorization');
 header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
 
 /*
@@ -17,13 +17,22 @@ header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/', function(Request $request){return json_encode(["status"=>true]);});
 
 Route::get('/groups', "Admin\GroupController@list");
 
 Route::post('/register', "RegisterController@registerUser");
 Route::post('/login', "LoginController@login");
 
-// Route::middleware('auth:api')->get('/user', function(Request $request){
-//     return $request->user();
-// });
+Route::get('/access_error', "UserController@accessError")->name('access_error');
+
+Route::middleware('auth:api')->group(function(){
+    Route::get('/user/access_level', "UserController@getUserAccessLevel");
+    Route::get('/user/userdata', "UserController@getAllUserData");
+    Route::get('/user/logout', "UserController@logout");
+});
+
+Route::middleware('auth:api')->get('/user', function(Request $request){
+    return $request->user();
+});
 
