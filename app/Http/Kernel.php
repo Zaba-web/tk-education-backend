8eaf74224547d525fc917745b2f13ad4066aef5a
Andriy <53307226+Zaba-web@users.noreply.github.com>
2021-07-12 21:13:30 +0300

    Delete midleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,8 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\AdminCheck::class,
-        'isOnline' => \App\Http\Middleware\OnlineCheck::class,
-        'apiToken' => \App\Http\Middleware\CheckApiToken::class
+        'isOnline' => \App\Http\Middleware\OnlineCheck::class
     ];
 
     /**
